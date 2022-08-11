4bc64d5706cf5c6cc8700e76f8e9ff576d25bd9f
Kenny0527 <Kenny0527@users.noreply.github.com>
2022-08-11 21:17:59 +0000

    studying for logic

---
+++ introfor/forloop2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+def main():
+    """RZFeeser | Alta3 Research
+        learning about for logic"""
+
+    # create the list called vendors
+    vendors = ["cisco", "juniper", "big_ip", "f5", "arista"]
+
+    # create a second list of strings
+    approved_vendors = ["cisco", "juniper", "nig_ip"]
+    count = 1
+    # CHALLENGE 1
+    farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+    for farm in farms:
+        print(farm.get("name"), end=":\n")
+        for industry in farm.get("agriculture"):
+            print(" " + str(count) + ") " + industry)
+            count += 1
+        count = 1
+
+    # loop across the list vendors
+    for x in vendors:
+        print("The vendor is:" + x)  # each time through the loop print value of x
+        if x not in approved_vendors:
+            print(" - NOT AN APPROVED VENDOR!", end="")
+    print("\nOur loop has ended.")  # when the loop ends print this
+
+if __name__ == "__main__":
+    main()
