e02e79e65b0692d5ad02dc1713bd78c7555b65f7
Kenny0527 <Kenny0527@users.noreply.github.com>
2022-08-11 16:45:26 +0000

    working on if, elif, else logic

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    """Alta3 Research | RZFreeser
+        Conditionals - strings test true"""
+    # testing the IP value
+    ipchk = "192.168.0.1"
+
+    # a string tests as True
+    if ipchk:
+        print("Looks like the UP addresses was set: " + ipchk)
+
+if __name__ == "__main__":
+    main()
