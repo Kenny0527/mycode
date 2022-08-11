e56725b28fcc763a93cd6234c15a09832a72f938
Kenny0527 <Kenny0527@users.noreply.github.com>
2022-08-11 16:22:03 +0000

    Learning about conditional if

---
+++ iftest/condition01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# create the string hostname
+hostname = "MTG"
+
+# test logic with the 'if' statement
+# what to do if this statement is found to be true
+if(hostname == "MTG"):
+    print("The hostname was found to be MTG")
