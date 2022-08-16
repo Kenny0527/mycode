701289dd5c09605e2de1db59902f4b53a0ad95e9
Kenny0527 <kenny0527@users.noreply.github.com>
2022-08-16 17:12:42 +0000

    learning about YAML, classproject updates

--- classproject/advertising.py
+++ classproject/advertising.py
@@ -5,19 +5,25 @@ import pandas as pd
 class Advertising:
     def __init__(self):
         self.directory = 'static/db'
-        self.advertising_data = []
+        self.advertising_data = readXlsx_to_dataFrame()
+        self.totalSpend = 0
+        self.totalSales = 0
+        self.overallAcos = 0
+
     # read xlsx files into container, xls to dictionary conversion
     def readXlsx_to_dataFrame(self):
+        ads = []
         for filename in os.scandir(self.directory):
             if filename.is_file():
-                self.advertising_data.append(pd.read_excel(filename))
-                print(self.advertising_data)
-        return None
-
-    def getAdvertising_data(self):
-        return self.advertising_data
+                ads.append(pd.read_excel(filename))
+                print(ads)
+        return ads
+    
         
-    # method that adds total spend
+    # method that gets total spend
+    def getTotalSpend(self):
+        return self.totalSpend
 
-# method that adds total sales
-# method that initializes spend, sales, acos, campaign insights
+    # method that gets total sales
+    def getTotalSales(self):
+        return self.totalSales
